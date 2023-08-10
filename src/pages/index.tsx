5768b6a7a172e68b614ce76a431e1b019f5a3552
axiangcoding <axiangcoding@gmail.com>
2023-08-10 16:55:57 +0800

    docs: 新增文档

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/quickstart">
+            to="/docs/document/quickstart">
               ⏱️ 用五分钟了解下AXBot
           </Link>
         </div>
